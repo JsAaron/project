52ddfbfb2133e0e7b03160c29965e47e43853ffc
jsAaron <401545553@qq.com>
2015-04-16 17:15:37 +0800

    update

--- node/test/formidable.js
+++ node/test/formidable.js
@@ -8,30 +8,22 @@ http.createServer(function(req, res) {
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
-      requestHandlers.upload(res,req)
+      requestHandlers.upload(res,
+          fields
+      )
       // res.writeHead(200, {
       //   'content-type': 'text/plain'
       // });
-      // res.write('received upload:\n\n');
-      // res.end(util.inspect({
-      //   fields: fields,
-      //   files: files
-      // }));
+      // res.write(
+      //     util.inspect({
+      //       fields: fields,
+      //       files: files
+      //     })
+      // );
+      // res.end();
     });
     return;
   }
-
   requestHandlers.start(res)
 
-  // res.writeHead(200, {
-  //   'content-type': 'text/html'
-  // });
-  // res.end(
-  //   '<form action="/upload" enctype="multipart/form-data" ' +
-  //   'method="post">' +
-  //   '<input type="text" name="title"><br>' +
-  //   '<input type="file" name="upload" multiple="multiple"><br>' +
-  //   '<input type="submit" value="Upload">' +
-  //   '</form>'
-  // );
 }).listen(3000)
