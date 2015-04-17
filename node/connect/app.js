28b343bd407fc1d9d101a09a342847e563ddb33a
jsAaron <401545553@qq.com>
2015-04-17 17:12:25 +0800

    connect

--- node/connect/app.js
+++ node/connect/app.js
@@ -1,10 +1,7 @@
 var connect = require('connect');
-
-	console.log(connect)
-
-
+connect.logger({ immediate: true, format: 'dev' })
 var app = connect()
-	.use(connect.logger('dev'))
-	.use(function(req, res) {
-		res.end('hello world\n');
-	}).listen(3000);
+    .use(function (req, res) {
+        res.end('hello world\n');
+    })
+    .listen(3000);
