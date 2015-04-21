1497b18467b8522ca834f5f4f1db8b31c671b741
jsAaron <401545553@qq.com>
2015-04-21 15:44:03 +0800

    movie

--- node/movie/app.js
+++ node/movie/app.js
@@ -1,28 +1,54 @@
 var express = require('express')
 var path = require('path')
-var mongoose = require('mongoose')
-var mongoStore = require('connect-mongo')(express)
 var port = process.env.PORT || 3000
 var app = express()
 
 
-app.set('views', './app/views/pages')
+app.set('views', './pages')
 app.set('view engine', 'jade')
 app.use(express.bodyParser())
-app.use(express.cookieParser())
-app.use(express.multipart())
-
-
-if ('development' === app.get('env')) {
-  app.set('showStackError', true)
-  app.use(express.logger(':method :url :status'))
-  app.locals.pretty = true
-  //mongoose.set('debug', true)
-}
-
+app.use(express.static(path.join(__dirname, 'bower_components')))
 app.listen(port)
-app.locals.moment = require('moment')
-app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'imooc 首页',
+		movies: [{
+			title: '机器1',
+			_id: 1,
+			poster: "https://ss1.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/super/whfpf=425,260,50/sign=8ccf579c063b5bb5be8273be50eee10b/b21bb051f81986189281b9b34fed2e738ad4e6c7.jpg"
+		}, {
+			title: '机器2',
+			_id: 2,
+			poster: "https://ss1.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/super/whfpf=425,260,50/sign=8ccf579c063b5bb5be8273be50eee10b/b21bb051f81986189281b9b34fed2e738ad4e6c7.jpg"
+		}]
+	})
+})
+
+
+app.get('/movie:id', function(req, res) {
+	res.render('index', {
+		title: 'movie 首页'
+	})
+})
+
+app.get('/admin/movie', function(req, res) {
+	res.render('index', {
+		title: 'imooc 后台'
+	})
+})
+
+
+app.get('/admin/list', function(req, res) {
+	res.render('index', {
+		title: 'imooc 列表'
+	})
+})
+
+
+
 
 console.log('imooc started on port ' + port)
 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
